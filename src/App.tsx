2b953c594dbc9f31e79bd5497b3e5695ad6d55ca
Amine <amineboussoualim@gmail.com>
2022-01-16 02:56:58 +0100

    fixed warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import GameBoard from './Components/GameBoard';
@@ -16,9 +14,6 @@ function App() {
       </div>
     </div>
   );
-  {/* Header*/}
-  {/* Gameboard */}
-  {/* Keyboard */}
 }
 
 export default App;
